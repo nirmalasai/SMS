5100be4068e1215221a0ef47b5059f7a41358d1f
MSDilliwal <marutisdilliwal97@gmail.com>
2022-08-04 12:38:41 +0530

    first commit by MS

--- com.tyss.studentmanagementframework/src/test/java/com/tyss/studentmanagement/Welcome.java
+++ com.tyss.studentmanagementframework/src/test/java/com/tyss/studentmanagement/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("WElcome to the Project");
+		System.out.println("hi everyone...");
 
 	}
 
